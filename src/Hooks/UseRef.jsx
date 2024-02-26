710ab10c68f908816c1b717a9ea7f0894087e410
OPTANWAR2001 <optanwar10@gmail.com>
2024-02-27 04:12:48 +0530

    add useRef hook

--- src/Hooks/UseRef.jsx
+++ src/Hooks/UseRef.jsx
@@ -6,13 +6,31 @@ const UseRef = () => {
   const handleFocus = () => {
     inputRef.current.focus();
   };
+
   return (
     <div>
       <h1 className="text-center my-5 text-4xl text-red-500">Use Ref Hooks</h1>
       <div className="w-1/2 mx-auto flex flex-col gap-10 justify-center">
-        <input type="text" ref={inputRef} className="outline-dashed"/>
-        <button onClick={handleFocus} className="border-2 p-4 inline">Focus</button>
+        <input type="text" ref={inputRef} className="outline-dashed" />
+        <button onClick={handleFocus} className="border-2 p-4 inline">
+          Focus
+        </button>
       </div>
+      <Container />
+    </div>
+  );
+};
+
+const Container = () => {
+  const divRef = useRef(null);
+  const handleColor = () => {
+    divRef.current.style.backgroundColor = "red";
+  };
+  return (
+    <div className="h-44 w-44 mx-auto my-24">
+      <button onClick={handleColor} ref={divRef}  className="border py-10 px-20">
+        Change Background Red
+      </button>
     </div>
   );
 };
